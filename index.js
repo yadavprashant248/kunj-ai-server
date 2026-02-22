d0c034662f2d297fdc31614039f9d96def45150d
Prashant <yadavprashant248@gmail.com>
2026-02-23 03:17:19 +0530

    Fix production PORT configuration

--- index.js
+++ index.js
@@ -177,6 +177,8 @@ ${question}
 // -----------------------------
 // START SERVER
 // -----------------------------
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
